83835f6fa2158f60a31a98e6a7254396e8e2a4d6
siffahim <siffahim25@gmail.com>
2025-04-17 00:17:51 +0600

    slove the max_min problem

---
+++ char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    scanf("%c", &ch);
+
+    if(ch >= 'a' && ch <= 'z'){
+        ch = ch - 32;
+        printf("%c", ch);
+    }else{
+        ch = ch + 32;
+        printf("%c", ch);
+    }
+
+    return 0;
+}
