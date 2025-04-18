5209dda9fcefa49fe779489ab6ab2d4104d98719
siffahim <siffahim25@gmail.com>
2025-04-18 23:36:28 +0600

    loop problem slove

--- digits.c
+++ digits.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int n;
+    int t;
+    scanf("%d", &t);
 
-    for (int i = 1; i <= 10; i++){
-        
+    for (int i = 1; i <= t; i++){
+        int n;
+        scanf("%d", &n);
+
+        do{
+            printf("%d ", n%10);
+            n /= 10;
+        } while (n != 0);
+        printf("\n");
     }
-        return 0;
+
+    return 0;
 }
