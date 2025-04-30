556dddffb240e6faeec5e85b34fdad8539b65dfa
siffahim <siffahim25@gmail.com>
2025-04-30 23:16:58 +0600

    slove the hackerrank contest proble 7

--- salami_again.c
+++ salami_again.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     int n;
     scanf("%d", &n);
+    int arr[n];
+    for (int i = 1; i <= n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int highestSalami = INT_MIN;
+
+    for (int i = 1; i <= n; i++){
+        if(arr[i] > highestSalami){
+            highestSalami = arr[i];
+        }
+    }
+
+    for (int i = 1; i <= n; i++){
+        arr[i] = highestSalami - arr[i];
+
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
