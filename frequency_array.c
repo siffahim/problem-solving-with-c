80b1e6b5700cbce1af497701077f3b95ab352002
siffahim <siffahim25@gmail.com>
2025-05-03 18:36:54 +0600

    slove sum the sum

--- frequency_array.c
+++ frequency_array.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(){
     int n, m;
     scanf("%d %d", &n, &m);
 
     int arr[n];
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i <= n; i++){
         scanf("%d", &arr[i]);
     }
 
-    int freq[5] = {0};
-
-    for (int i = 0; i < 5; i++){
-        freq[i]++;
+    int freq[m];
+    for (int i = 1; i <= m; i++){
+        freq[i] = 0;
     }
+    
+    for (int i = 1; i <=n; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    for (int i = 1; i <= m; i++){
+        printf("%d\n", freq[i]);
+    }
+
 
     return 0;
 }
+
