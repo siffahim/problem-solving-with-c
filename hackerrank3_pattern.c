c60574279636723405627ddba871683f6f9095c3
siffahim <siffahim25@gmail.com>
2025-05-09 14:34:58 +0600

    slove the problem

--- hackerrank3_pattern.c
+++ hackerrank3_pattern.c
@@ -5,14 +5,13 @@ int main() {
     scanf("%d", &n);
     int hash = 1;
     int space = n - 1;
-
-    // Top half pattern
+    
+    
     for (int i = 1; i <= n; i++) {
-        // Print spaces
         for (int k = 1; k <= space; k++) {
             printf(" ");
         }
-        // Print hashes or dashes based on row
+    
         if (i % 2 == 1) {
             for (int j = 1; j <= hash; j++) {
                 printf("#");
@@ -22,12 +21,35 @@ int main() {
                 printf("-");
             }
         }
-
         printf("\n");
-        hash += 2;  // Increase the number of symbols by 2
-        space--;  // Decrease the spaces
+        
+    
+        hash += 2;
+        space--;
     }
 
-    
+    //ultra print
+    hash -= 4;
+    space = 1;  
+
+    for (int i = n-1; i >= 1; i--) {
+        for (int k = 1; k <= space; k++) {
+            printf(" ");
+        }
+        if (i % 2 == 1) {
+            for (int j = 1; j <= hash; j++) {
+                printf("#");
+            }
+        } else {
+            for (int j = 1; j <= hash; j++) {
+                printf("-");
+            }
+        }
+        printf("\n");
+
+        hash -= 2;
+        space++;
+    }
+
     return 0;
 }
